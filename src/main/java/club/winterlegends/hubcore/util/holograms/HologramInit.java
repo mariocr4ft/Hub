01709563c5b53c0e56115762c047712b8c64dc88
Mariocraft <mariocraftuwu@gmail.com>
2024-08-10 15:27:34 +0200

    Made a refresh on start

--- src/main/java/club/winterlegends/hubcore/util/holograms/HologramInit.java
+++ src/main/java/club/winterlegends/hubcore/util/holograms/HologramInit.java
@@ -1,15 +1,15 @@
 package club.winterlegends.hubcore.util.holograms;
 
+import club.winterlegends.hubcore.HubCore;
 import eu.decentsoftware.holograms.api.holograms.Hologram;
 import eu.decentsoftware.holograms.api.holograms.HologramManager;
-import club.winterlegends.hubcore.HubCore;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 import java.util.Collection;
 
 public class HologramInit {
-    public static HologramManager MANAGER;
+    private static eu.decentsoftware.holograms.api.DecentHolograms DecentHolograms;
     public static HubCore MAIN;
     public static boolean ENABLED;
 
@@ -18,15 +18,17 @@ public class HologramInit {
     }
 
     private void load() {
-        Collection<Hologram> hologramList = MANAGER.getHolograms();
         if (HubCore.CONFIG.getBoolean("HOLOGRAMS")) {
             if (Bukkit.getPluginManager().getPlugin("DecentHolograms") != null || !Bukkit.getPluginManager().getPlugin("DecentHolograms").isEnabled()) {
                 try {
-                    for (Hologram hologram : hologramList) {
-                        hologram.hideAll();
-                        hologram.showAll();
-                        MANAGER.reload();
-                    }
+                        HologramManager manager = new HologramManager(DecentHolograms);
+                        DecentHolograms.reload();
+                        Collection<Hologram> holograms = manager.getHolograms();
+                        for (Hologram hologram : holograms) {
+                            hologram.register();
+                            hologram.save();
+                        }
+                        
                 } catch (Exception e) {
                     e.printStackTrace();
                     Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_RED + "Holograms couldn't load properly. Please check decentholograms version.");
